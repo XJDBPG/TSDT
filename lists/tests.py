c6f130477319fff30f6823418aac87ca6fc73564
XJDBPG <chenjiarui0305@foxmail.com>
2024-03-28 15:44:20 +0800

    message 6: Functional test now checks we can input a to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
